174e4a865347e0e49a10de78ff98f465b7b77473
Aditya <adityabhattacharjee00@gmail.com>
2026-07-08 21:10:49 +0530

    Updating backend file

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "your-tfstate-bucket-name"
+    bucket       = "terraform-remote-backend-720277889761-ap-south-1-an"
     key          = "eks-learning/dev/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
